338961505f4aba841208e06622cc6a1dc1bc5c74
Than Duc Thang <ducthang200023@gmail.com>
2020-09-01 18:20:22 +0700

    fix task 1

--- taskA.js
+++ taskA.js
@@ -8,31 +8,25 @@ let checkRemainder = (num) =>{
 let divideByTwo = (arr,f) => {
     let newArr = []
     arr.forEach(element => {
-        element/=2
-        if(checkRemainder(element)){
+        let temp = element/2
+        if(f(temp)){
            newArr.push(element)
         }
     });
     return newArr
 }
 
-console.log(divideByTwo([1,2,3,4,5,6], checkRemainder))
+console.log(divideByTwo([1,2,3,4,5,6,7,8,9,10], checkRemainder))
 
-//Below cover solution for task 2
+//Belows cover solution for task 2
 let deDuplicate = (arr1,arr2) =>{
-    result = []
     arr1.forEach(element =>{
-        if(!arr2.includes(element)){
-            result.push(element)
-        }
-    })
-    arr2.forEach(element =>{
-        if(!arr1.includes(element)){
-            result.push(element)
+        if(arr2.indexOf(element) == -1) {
+            arr2.push(element);
         }
     })
 
-    return result
+    return arr2
 }
 
-console.log(deDuplicate([1,2,"a"],[1,3,"b"]))
+console.log(deDuplicate([1,2,"a","b",`c`],[1,2,3,4,5,6]))
